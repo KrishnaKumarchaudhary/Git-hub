31a2558007ed3901a0fa8894a867d22b3838c5ca
Krishna kumar chaudhary <ch.krishnakumar01@gmail.com>
2022-01-14 22:03:05 +0530

    3 comment

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 const Error = () => {
-  return <h2>error page</h2>;
+  return (
+    <Wrapper>
+      <div>
+        <h1>404</h1>
+        <h3>Sorry, The Page You Tried Cannot Be Found</h3>
+        <Link to="/" className="btn">
+          Back Home
+        </Link>
+      </div>
+    </Wrapper>
+  );
 };
 const Wrapper = styled.section`
   min-height: 100vh;
@@ -18,4 +28,19 @@ const Wrapper = styled.section`
     margin-bottom: 1.5rem;
   }
 `;
+
+// const Wrapper = styled.section`
+//   min-height: 100vh;
+//   display: grid;
+//   place-items: center;
+//   background: var(--clr-primary-10);
+//   text-align: center;
+//   h1 {
+//     font-size: 10rem;
+//   }
+//   h3 {
+//     color: var(--clr-grey-3);
+//     margin-bottom: 1.5rem;
+//   }
+// `;
 export default Error;
